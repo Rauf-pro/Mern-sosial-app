2a8ab7216003fc58675050490321243d4cdca275
rrauf8178 <rrauf8178@gmail.com>
2023-10-14 00:20:07 +0400

    client: install react-router-dom as dependencies and configured app root

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,20 @@
 import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
+import Profile from "./pages/Profile/Profile";
+import Register from "./pages/Register/Register";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useContext } from "react";
 
 function App() {
   return (
-    <div>
-      <Home />
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </Router>
   );
 }
 
